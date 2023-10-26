e2e8f074522a099024fed00d934bd427bda47c6d
Uzair <uzam.dev@gmail.com>
2023-10-26 19:22:52 +0500

    updates

--- sqlalchemy_facade/__init__.py
+++ sqlalchemy_facade/__init__.py
@@ -1,4 +1,4 @@
 from sqlalchemy_facade.src.read_repository import ReadRepository
 from sqlalchemy_facade.src.write_repository import WriteRepository
 from sqlalchemy_facade.src.ddl_repository import DDLRepository
-from sqlalchemy_facades.src.declarative_base import Base
+from sqlalchemy_facade.src.declarative_base import Entity
--- sqlalchemy_facade/src/__init__.py
+++ sqlalchemy_facade/src/__init__.py
@@ -1,4 +1,4 @@
 from src.ddl_repository import DDLRepository
 from src.read_repository import ReadRepository
 from src.write_repository import WriteRepository
-from src.declarative_base import Base
+from src.declarative_base import Entity
